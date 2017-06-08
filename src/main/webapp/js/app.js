3370538bc65c8ee09a9c4df5d14f730b6349cd04
vtroyanov <Viacheslav.Troyanov@artezio.com>
2017-06-08 18:16:24 +0300

    Delete records

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -7,8 +7,31 @@ angular.module("accountManager", [])
         //$scope.users = [{"login":"login2","displayName":"User User 2","password":"password2","birthday":1496916396207,"gender":false},{"login":"login1","displayName":"User User 1","password":"password1","birthday":1496916396206,"gender":true}];
         var query = "/JaxRsMicroservice/rest/users/";
 
-        $http.get(query).
-            then(function(response) {
+        $scope.reload = function() {
+            $http.get(query).then(function (response) {
                 $scope.users = response.data;
             });
+        };
+
+        $scope.add = function(personalDetail){
+        };
+
+        $scope.save = function(user){
+            if (user.login) {
+                //TODO
+            } else {
+                //warning
+            }
+        };
+
+        $scope.delete = function(user) {
+            if (confirm('Вы действительно хотите удалить запись?')) {
+                $http.delete(query + user.login)
+                    .then(function (response) {
+                        $scope.reload();
+                    });
+            }
+        };
+
+        $scope.reload();
     });
